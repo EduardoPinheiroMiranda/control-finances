cdc3cd705860b5b6cfead993c5bd4a8b0f1831a9
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-01-22 22:01:27 -0300

    finalizando componente de grafico

--- src/pages/home/consumptionIndicator/index.js
+++ src/pages/home/consumptionIndicator/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { stylesDefault } from "../../../themes/stylesDefault";
+import { colors } from "../../../themes";
+import { styles } from "./styles";
+
+
+import { ExpenseIndicator } from "../../../components/expenseIndicator";
 
 
 export function ConsumptionIndicator(){
 
+    const [expiration, setExpiration] = useState("10/02");
+
 
     return(
-        <View style={[stylesDefault.box]}>
-            <Text>
-                oi
+        <View style={[stylesDefault.box, styles.container]}>
+            <Text style={{fontSize: 16, color: colors.color_4}}>
+                Vencimento - {expiration}
             </Text>
+
+            <View>
+                <ExpenseIndicator data={{value: 80, size: 150, strokeWidth: 10}}/>
+                <View>
+                    
+                </View>
+            </View>
+            
         </View>
     );
 }
