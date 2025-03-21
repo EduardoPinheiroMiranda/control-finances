feb1e50fb60e270e4415365b05291e4154d4a6b8
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-03-21 17:25:24 -0300

    adicionando estilo ao modal

--- src/pages/Movements/filter/index.js
+++ src/pages/Movements/filter/index.js
@@ -1,15 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { styles } from "./styles";
+import { defaultPageStyle } from "../../../themes/stylesDefault";
 
 
 export function Filter(){
 
+    const [date, setDate] = useState("");
+    const [type, setType] = useState("");
 
     return(
-        <View>
-            <Text>
-                oi filtro
-            </Text>
+        <View style={styles.container}>
+
+            <View style={styles.modal}>
+
+            <View style={styles.sectionInput}>
+                    <Text style={[defaultPageStyle.text, styles.label]}>Período</Text>
+                    <TextInput 
+                        style={styles.input}
+                        value={date}
+                        onChangeText={(valu) => setDate(valu)}
+                        placeholder="01/01/2025"
+                    />
+                </View>
+
+                <View style={styles.sectionInput}>
+                    <Text style={[defaultPageStyle.text, styles.label]}>Tipo de movimentação</Text>
+                    <TextInput 
+                        style={styles.input}
+                        value={date}
+                        onChangeText={(valu) => setDate(valu)}
+                        placeholder="01/01/2025"
+                    />
+                </View>
+
+            </View>
+            
         </View>
     );
 }
--- src/pages/Movements/filter/styles.js
+++ src/pages/Movements/filter/styles.js
@@ -1,8 +1,47 @@
 import { StyleSheet } from "react-native";
+import { colorPattern } from "../../../themes";
 
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        position: "relative"
+    },
+    modal:{
+        position: "absolute",
+        bottom: 0,
+
+        backgroundColor: colorPattern.white_800,
+
+        width: "100%",
+        height: 200,
+
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+
+        borderRadius: 20,
+        elevation: 10
+    },
+
+    sectionInput: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 10
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: "regular",
+        color: colorPattern.black_900
+    },
+    input: {
+        width: 180,
+
+        fontSize: 16,
+        textAlign: "center",
+
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: colorPattern.gray_300
     }
 })
