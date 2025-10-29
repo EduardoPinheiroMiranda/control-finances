f70f5ad0ee73be860f08f8b9adb8aaada711dcc7
Eduardo Pinheiro Miranda <pinheiomiranda@gmail.com>
2025-10-29 11:39:50 -0300

    finalizando refresh na pagina de movimentações

--- src/pages/Movements/index.tsx
+++ src/pages/Movements/index.tsx
@@ -1,15 +1,24 @@
-import { Container } from "./styles";
-import { useContext, useEffect } from "react";
+import { Container, Spiner } from "./styles";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/contexts/user.context";
 
 // components
 import { Movement } from "@/components/Movement";
 import { FlatList } from "react-native";
+import { externalCalls } from "@/services/externalCalls";
+import axios from "axios";
+import { AuthContext } from "@/contexts/Auth.context";
 
 
 export function Movements(){
 
 	const userContext = useContext(UserContext);
+	const authContext = useContext(AuthContext);
+
+	const [movements, setMovements] = useState(userContext?.movements);
+	const [loading, setLoading] = useState(false);
+	
+	if (!userContext?.movements) return;
 
 
 	useEffect(() => {
@@ -22,18 +31,39 @@ export function Movements(){
 	}, []);
 
 
-	if (!userContext?.movements) return;
+	async function refresheData(){
+
+		try{
+			
+			setLoading(true);
+			const cursor = movements?.length ?? 0;
+			const request = await externalCalls.get(`/movement/getAllMovements?cursor=${cursor}`);
+			const response = request.data;
+			setMovements(!movements ? response : [...movements, ...response]);
+			setLoading(false);
+
+		}catch(err){
+			setLoading(false);
+			if(axios.isAxiosError(err)){
+				if(err.status === 401){
+					return authContext?.singOut();
+				}
+			}
+		}
+	}
 
 
 	return(
 		<Container>
 			<FlatList
 				style={{flex: 1}}
-				data={userContext.movements}
+				data={movements}
 				renderItem={({item}) => <Movement movement={item}/>}
 				keyExtractor={(item) => item.id}
 				horizontal={false}
 				showsVerticalScrollIndicator={false}
+				onEndReached={() => refresheData()}
+				ListFooterComponent={() => loading && <Spiner/>}
 			/>
 		</Container>
 	);
--- src/pages/Movements/styles.tsx
+++ src/pages/Movements/styles.tsx
@@ -11,3 +11,8 @@ export const Container = styled.View`
     background-color: ${({theme}: ThemeTypes) => theme.colors.BACKGROUND_PRIMARY};
     flex: 1;
 `;
+
+export const Spiner = styled.ActivityIndicator.attrs(({theme}: ThemeTypes) => ({
+	color: theme.colors.SECONDARY,
+	size: 20,
+}))``;
